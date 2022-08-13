99f23638accd044c3d5f91355cc5be09acf2e809
icelancer <sendtoghkim@gmail.com>
2022-08-13 20:30:58 +0900

    initial commit

---
+++ src/test/kotlin/com/icelancer/batch/SpringBatchExamplesApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.icelancer.batch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBatchExamplesApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
